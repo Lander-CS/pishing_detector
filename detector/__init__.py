4447202862ff55314718a1115983af605ca60c18
Lander-CS <aragaolanderson08@gmail.com>
2026-03-16 16:06:37 -0300

    Criei o vercel.json

--- detector/__init__.py
+++ detector/__init__.py
@@ -2,4 +2,3 @@ from .url_analyzer import analyze_url
 from .domain_analyzer import analyze_domain
 from .html_analyzer import analyze_html
 from .typosquat_detector import detect_typosquatting
-from .domain_analyzer import analyze_domain_indicators
